80524fe95ff2d435a01b7ab9097a17b9babc0e35
Saam97 <samuel_dacruz@hotmail.com>
2021-05-13 22:02:22 -0300

    create function 'get_data' to load datasets

--- get_data.py
+++ get_data.py
@@ -1,37 +1,63 @@
-import requests
 import json
 import os
 from dotenv import load_dotenv
 import pandas as pd
-from datetime import datetime
+import requests
+from pytictoc import TicToc
 
 
-# Busca os dados da API
-load_dotenv()
+def get_data(coin, sample_data=True):
 
-ENDPOINT_BTC = os.getenv('ENDPOINT_BTC')
+    # Verificação dos parâmetros #
+    if coin.upper() not in ('BTC', 'ETH', 'XRP'):
+        err_msg = coin + ' is a invalid coin!'
+        raise ValueError(err_msg)
 
+    # Variáveis de Ambiente #
+    # Nome da variável de Ambiente
+    name_coin = "_SAMPLE_DATA" if sample_data else "_ALL_DATA"
+    name_coin = coin.upper() + name_coin
 
-def convertToDF(dfJSON):
-    return(pd.json_normalize(dfJSON))
+    print("\nGetting ", "sample" if sample_data else "all",
+        " data from ", coin.upper(), " coin ...")
 
+    # Carre as variáveis de ambiente
+    load_dotenv()
 
-def buscar_dados():
-    request = requests.get(ENDPOINT_BTC)
-    todo = json.loads(request.content)
-    return todo['Data']['Data']
+    # Tempo de processamento
+    t = TicToc()
+    t.tic()
 
+    # Busca pela variável da moeda selecionada
+    coin_url = os.getenv(name_coin)
 
-if __name__ == '__main__':
-    data = buscar_dados()
-    df = convertToDF(data)
+    # Request na API #
+    # Realiza a Request
+    request = requests.get(coin_url)
+    # Pega os dados vindos da Request
+    data = json.loads(request.content)
+    # Pega o campo "Data"
+    content = data.get("Data")
+    # Pega novamente o segundo campo "Data"
+    content = content.get("Data")
 
-    print(df.head())
+    print("Dataset has been loaded! Formatting ...")
 
-    df = df[['time', 'close']]
+    # Manipulação dos dados #
+    # Primeira observação
+    df = pd.json_normalize(content[0])
 
-    df.columns = ['DS', 'Y']
+    # Para cada linha da lista
+    for i in range(1, len(content)):
+        # Pega a observação (dicionário)
+        observation = content[i]  
 
-    df['DS'] = datetime.fromtimestamp(df['DS'])
+        # Transforma em um DataFrame
+        df_temp = pd.json_normalize(observation)
 
-    print(df.head())
+        # Concatena com o DataFrame inicial
+        df = pd.DataFrame.append(df, df_temp)
+
+    t.toc("Done!")
+    # Retorna o DataFrame com as observações
+    return df
